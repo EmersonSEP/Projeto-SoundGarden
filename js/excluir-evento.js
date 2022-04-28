317af0e9871809b23eb18b755cecf5d1315712f4
StaticDreamstate <lonely_loonie@protonmail.com>
2022-04-27 21:34:50 -0300

    excluir-evento.js - refatoração.

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -1,11 +1,4 @@
 
-const eventoNome = document.getElementById("nome");
-const eventoAtracoes = document.getElementById("atracoes");
-const novasAtracoes = eventoAtracoes.value.split(",");
-const eventoDescricao = document.getElementById("descricao");
-const eventoDataHora = document.getElementById("data");
-const eventoLotacao = document.getElementById("lotacao");
-
 const btnExcluir = document.querySelector("button.btn.btn-danger");
 
 const eventoId = (new URL(document.location)).searchParams.get("id");
