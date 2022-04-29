435b60c7a9b1d6c952e6d843f7612b16d15ea1e0
StaticDreamstate <lonely_loonie@protonmail.com>
2022-04-28 21:44:47 -0300

    Banners definitivos.

--- js/banner.js
+++ js/banner.js
@@ -1,14 +1,14 @@
 function slide1(){
-    document.getElementById('id').src="img/banner1.jpg";
+    document.getElementById('id').src="img/ban1.jpg";
     setTimeout("slide2()", 2000);
     }
     
     function slide2(){
-    document.getElementById('id').src="img/banner2.jpg";
+    document.getElementById('id').src="img/ban2.jpg";
     setTimeout("slide3()", 2000);
     }
     
     function slide3(){
-    document.getElementById('id').src="img/banner3.jpg";
+    document.getElementById('id').src="img/ban3.jpg";
     setTimeout("slide1()", 2000);
     }
