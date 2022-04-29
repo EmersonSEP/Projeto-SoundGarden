61126b2a4b074d9c44dcbc5379771f5f5b9bb7e6
StaticDreamstate <lonely_loonie@protonmail.com>
2022-04-28 22:17:33 -0300

    Campos preenchidos em excluir-evento.js

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -1,7 +1,34 @@
 
+const eventoNome = document.getElementById("nome");
+const eventoBanner = document.getElementById("banner");
+const eventoAtracoes = document.getElementById("atracoes");
+const eventoDescricao = document.getElementById("descricao");
+const eventoDataHora = document.getElementById("data");
+const eventoLotacao = document.getElementById("lotacao");
+
 const btnExcluir = document.querySelector("button.btn.btn-danger");
 
 const eventoId = (new URL(document.location)).searchParams.get("id");
+
+fetch("https://xp41-soundgarden-api.herokuapp.com/events")
+    .then(data => data.json()) 
+    .then(eventos => { 
+        eventos.forEach(evento => { 
+            if(evento._id == eventoId) { 
+
+                const dataN = new Date(evento.scheduled);
+                const dataFormatada = dataN.toLocaleDateString();
+
+                eventoNome.value = evento.name;
+                eventoBanner.value = evento.poster;
+                eventoAtracoes.value = evento.attractions.join(', ');
+                eventoDescricao.value = evento.description;
+                eventoDataHora.value = evento.scheduled.substring(0, evento.scheduled.length-1);
+                eventoLotacao.value = evento.number_tickets;
+            }
+        });
+    })
+    .catch(err => console.error(err));
 
 btnExcluir.addEventListener("click", async (event) => {
 
